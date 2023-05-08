9cd9225bb1354d99dda2c2a56f93c2af9e8df9eb
ChrisResl <christian-resl@gmx.at>
2023-05-08 15:54:04 +0200

    argument parser + testing file structure

--- 2FolderSynchronizer.py
+++ 2FolderSynchronizer.py
@@ -1,7 +1,19 @@
+import argparse
+
+
+def argparser():
+    """Store user input and return as Namespace object"""
+    parser = argparse.ArgumentParser(prog="2FolderSynchronizer", description="Synchronizing content of input folders")
+    parser.add_argument("-s", "--source", required=True, help="Path of Source folder")
+    parser.add_argument("-r", "--replica", required=True, help="Path of Replica folder")
+    parser.add_argument("-l", "--log", required=True, help="Output file path to log file")
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    pass
+    p = argparser()
 
 
 if __name__ == "__main__":

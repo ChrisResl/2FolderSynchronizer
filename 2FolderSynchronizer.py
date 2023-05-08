96a4219bd82c4a2d22bee163baae866db922b173
ChrisResl <christian-resl@gmx.at>
2023-05-08 16:06:29 +0200

    synchronization interval argument

--- 2FolderSynchronizer.py
+++ 2FolderSynchronizer.py
@@ -6,6 +6,7 @@ def argparser():
     parser = argparse.ArgumentParser(prog="2FolderSynchronizer", description="Synchronizing content of input folders")
     parser.add_argument("-s", "--source", required=True, help="Path of Source folder")
     parser.add_argument("-r", "--replica", required=True, help="Path of Replica folder")
+    parser.add_argument("-i", "--interval", nargs="?", default=1, help="Synchronization interval")
     parser.add_argument("-l", "--log", required=True, help="Output file path to log file")
 
     args = parser.parse_args()
